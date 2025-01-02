dc50a5a14e9e00bd94ec896cb73bfafbcfa724cf
Yasaswini <yasha.balu25@gmail.com>
2025-01-02 21:29:00 +0530

    App creation and initial changes. Added app header.

---
+++ src/components/Header.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+type HeaderProps = {
+    
+};
+
+const Header:React.FC<HeaderProps> = () => {
+    
+    return <header>
+        <h1>My Journal</h1>
+    </header>
+}
+export default Header;
